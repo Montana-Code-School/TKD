e7cb644a7a2087dc624ae7f3f576e1ac17d69995
Mike Dreiling <mdarts@gmail.com>
2017-08-08 19:32:50 -0600

    build Home component

---
+++ TKD-client/src/components/Home.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Header from './Header';
+import ColoredNav from './ColoredNav';
+import Gmap from './Gmap';
+import headerImg from '../img/homeMainImg.jpg';
+
+export default class Home extends React.Component {
+
+
+
+
+render() {
+  return (
+    <div className='container'>
+      <Header />
+      <img className="mainImg" src={headerImg} alt="Saja Academy" />
+      <h1>Saja Academy</h1>
+      <h2>Tae Kwon Do For The Whole Family</h2>
+      <ColoredNav />
+
+      <div className="eventLog">
+        <div className="eventLogContent">
+          <h2>Brother Schools</h2>
+            <ul>
+              <li>School 1 Goes Here</li>
+              <li>School 2 Goes Here</li>
+              <li>School 3 Goes Here</li>
+              <li>School 4 Goes Here</li>
+              <li>School 5 Goes Here</li>
+            </ul>
+        </div>
+
+        <div className="eventLogContent">
+          <h2>Upcoming Events</h2>
+            <ul>
+              <li>Event 1 Goes Here</li>
+              <li>Event 2 Goes Here</li>
+              <li>Event 3 Goes Here</li>
+              <li>Event 4 Goes Here</li>
+              <li>Event 5 Goes Here</li>
+            </ul>
+        </div>
+
+        </div>
+
+        <div className='mapSection'>
+          <Gmap />
+          <div className='contactBox'>
+            <h2>105 Ravalli Street</h2>
+            <h2>Stevensville, MT 59870</h2><br/>
+            <h2>Monday 5PM - 7PM</h2>
+            <h2>Tuesday 5PM - 6:30PM</h2>
+            <h2>Friday 5PM - 7PM</h2><br/>
+            <h2>406.210.3164</h2>
+            <h2>sajaacademyma@gmail.com</h2>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+}
