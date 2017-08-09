a4eaf45b9dab5dc92f71a57b6059ac1af1b4edb9
Brigitta <baltwo@gmail.com>
2017-08-09 10:44:20 -0600

    added comments, photo of April to about page, colored nav images to Colored Nav component, gallery images to img folder

--- TKD-client/src/components/ColoredNav.js
+++ TKD-client/src/components/ColoredNav.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import oneRedSquare from '../img/home_Squares1.jpg';
+import oneBlueSquare from '../img/home_Squares2.jpg';
+import twoRedSquare from '../img/home_Squares3.jpg';
+import twoBlueSquare from '../img/home_Squares4.jpg';
 
 export default class ColoredNav extends React.Component {
-
-
 
   render() {
     return (
       <div>
-        Colored Squares Here
+        <img src={oneRedSquare} alt="about" />
+        <h2>About</h2>
+        <img src={oneBlueSquare} alt="calendar" />
+        <h2>Calendar</h2>
+        <img src={twoRedSquare} alt="registration" />
+        <h2>Registration</h2>
+        <img src={twoBlueSquare} alt="gallery" />
+        <h2>Gallery</h2>
       </div>
     );
   }
