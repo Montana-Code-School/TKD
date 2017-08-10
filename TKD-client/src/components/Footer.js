6fb0aa2e43a49c65aea5258b37f33ae4261635e0
Mike Dreiling <mdarts@gmail.com>
2017-08-09 23:46:26 -0600

    footer structured

--- TKD-client/src/components/Footer.js
+++ TKD-client/src/components/Footer.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import '../styles/Footer.css';
+import fbIcon from '../img/footer_faceBookIcon.png';
 
 export default class Footer extends React.Component{
   render() {
-    return <h1>Footer!</h1>
+    return (
+      <div className="navContainer">
+        <ul className="footerNav">
+          <li>HOME</li>
+          <li>EVENTS</li>
+          <li>REGISTRATION</li>
+          <li>GALLERY</li>
+          <li>ABOUT</li>
+          <li><img src={fbIcon} alt="Link To Saja Facebook Page" /></li>
+        </ul>
+      </div>
+    );
   }
 }
