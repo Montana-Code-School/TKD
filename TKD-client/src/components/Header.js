cab1851b04fca39ab76381058fc9cdeb6228f3ed
Brigitta <baltwo@gmail.com>
2017-08-10 18:59:58 -0600

    added router

--- TKD-client/src/components/Header.js
+++ TKD-client/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import navImgOne from '../img/navBar_KoreanFlag.png';
 import navImgTwo from '../img/Jungdo.png';
 import '../styles/Header.css';
@@ -9,17 +10,17 @@ export default class Header extends React.Component {
       <header className="navContainer">
         <ul className="row1">
           <li className="row1column1">email@email.com</li>
-          <li className="row1column2">FAQ</li>
-          <li className="row1column3">PAY YOUR MEMBERSHIP</li>
+          <Link to="/FAQ"><li className="row1column2">FAQ</li></Link>
+          <Link to="/Registration"><li className="row1column3">PAY YOUR MEMBERSHIP</li></Link>
         </ul>
         <ul className="row2">
           <li className="row2column1"><img src={navImgOne} alt="" /></li>
           <li className="row2column2"><img src={navImgTwo} alt="" /></li>
-          <li className="row2column3">Home</li>
-          <li className="row2column4">Events</li>
-          <li className="row2column5">Registration</li>
-          <li className="row2column6">Gallery</li>
-          <li className="row2column7">About</li>
+          <Link to="/"><li className="row2column3">Home</li></Link>
+          <Link to="/Calendar"><li className="row2column4">Calendar</li></Link>
+          <Link to="/Registration"><li className="row2column5">Registration</li></Link>
+          <Link to="/Gallery"><li className="row2column6">Gallery</li></Link>
+          <Link to="/About"><li className="row2column7">About</li></Link>
         </ul>
       </header>
     );
