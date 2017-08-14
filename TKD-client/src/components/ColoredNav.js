0a1486629c7f3f17d9f2f24ad5708606462a38dc
Mike Dreiling <mdarts@gmail.com>
2017-08-14 07:08:10 -0600

    Home page fully styled

--- TKD-client/src/components/ColoredNav.js
+++ TKD-client/src/components/ColoredNav.js
@@ -3,21 +3,30 @@ import oneRedSquare from '../img/home_Squares1.jpg';
 import oneBlueSquare from '../img/home_Squares2.jpg';
 import twoRedSquare from '../img/home_Squares3.jpg';
 import twoBlueSquare from '../img/home_Squares4.jpg';
+import '../styles/Home.css';
 
 export default class ColoredNav extends React.Component {
 
   render() {
     return (
-      <div>
-        <img src={oneRedSquare} alt="about" />
-        <h2>About</h2>
-        <img src={oneBlueSquare} alt="calendar" />
-        <h2>Calendar</h2>
-        <img src={twoRedSquare} alt="registration" />
-        <h2>Registration</h2>
-        <img src={twoBlueSquare} alt="gallery" />
-        <h2>Gallery</h2>
-      </div>
+      <ul className="coloredNavContainer">
+        <li className="navSquare">
+          <img src={oneRedSquare} alt="about" />
+          <h2>About</h2>
+        </li>
+        <li className="navSquare">
+          <img src={oneBlueSquare} alt="calendar" />
+          <h2>Calendar</h2>
+        </li>
+        <li className="navSquare">
+          <img src={twoRedSquare} alt="registration" />
+          <h2>Registration</h2>
+        </li>
+        <li className="navSquare">
+          <img src={twoBlueSquare} alt="gallery" />
+          <h2>Gallery</h2>
+        </li>
+      </ul>
     );
   }
 }
