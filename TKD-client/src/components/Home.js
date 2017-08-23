a656c9e7414d8c8367003e117b2b8bf26500b69b
April Trull <mtwoolybunnies@gmail.com>
2017-08-23 03:16:04 +0100

    added styling and info to to brother schools and upcoming events divs

--- TKD-client/src/components/Home.js
+++ TKD-client/src/components/Home.js
@@ -25,23 +25,22 @@ export default class Home extends React.Component {
             <h2>Brother Schools</h2>
             <hr />
               <ul>
-                <li>Parks Martial Arts Academy  1920 Central Ave. Billings, MT 59102  (406)245-8808</li>
-                <li>Cody Martial Arts 502 Ray Rd Cody, WY 82414 (307)527-7613</li>
-                <li>AJF Martial Arts Academy-Ranchester 103 Carl St. Ranchester, WY 82839 (307)655-9761</li>
-                <li>AJF Martial Arts-Sheridan 1030 N Main St. Sheridan, WY 82801 (307)751-5540</li>
-                <li>Parks Martial Arts Academy-Hardin 121 N Mitchell Ave. Hardin, MT 59034 (406)679-1874</li>
-                <li>AJF Martial Arts-Pierre 203 E Sioux Ave Pierre S.Dakota 57501 (605)295-0352</li>
+                <li>Parks Martial Arts Academy<br/>1920 Central Ave. Billings, MT 59102 <br/> (406)245-8808</li><br/>
+                <li>Cody Martial Arts <br/>502 Ray Rd Cody, WY 82414 <br/>(307)527-7613</li><br/>
+                <li>AJF Martial Arts Academy-Ranchester <br/>103 Carl St. Ranchester, WY 82839 <br/>(307)655-9761</li><br/>
+                <li>AJF Martial Arts-Sheridan <br/>1030 N Main St. Sheridan, WY 82801<br/> (307)751-5540</li><br/>
+                <li>Parks Martial Arts Academy-Hardin <br/>121 N Mitchell Ave. Hardin, MT 59034<br/> (406)679-1874</li><br/>
+                <li>AJF Martial Arts-Pierre <br/>203 E Sioux Ave Pierre S.Dakota 57501 <br/>(605)295-0352</li>
               </ul>
           </div>
           <div className="eventLogContent">
             <h2>Upcoming Events</h2>
             <hr />
               <ul>
-                <li>Event 1 Goes Here</li>
-                <li>Event 2 Goes Here</li>
-                <li>Event 3 Goes Here</li>
-                <li>Event 4 Goes Here</li>
-                <li>Event 5 Goes Here</li>
+                <li>September 22, 2017  5pm   Color Belt Test  at Saja Academy</li><br/>
+                <li>October 6, 2017 6pm  Black Belt Test  in Cody Wyoming</li><br/>
+                <li>October 7, 2017 9am Cody Martial Art Invitaion Tournament  in Cody Wyoming</li>
+
               </ul>
           </div>
         </div>
