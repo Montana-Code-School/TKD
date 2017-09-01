c306df495e05e781d8b0898017a6cb235dc91309
April Trull <mtwoolybunnies@gmail.com>
2017-09-01 03:16:20 +0100

    yep

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use((req, res, next) =>{
   next();
 });
 
-const host = "localhost"
-const user = "root"
-const pswd = "13fuzzybunnys"
-const dbname = "saja_academy"
+const host = "us-cdbr-iron-east-05.cleardb.net"
+const user = "bed60c6ac47294"
+const pswd = "b11032f8"
+const dbname = "heroku_dd8592f0f69936c"
 
 // config db ====================================
 const connection = mysql.createConnection({
@@ -28,8 +28,9 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-app.get('/student/:studentemail', (req, res) => {
-  const userQuery = "SELECT user.id FROM saja_academy.user WHERE user.email =" + connection.escape(req.params.studentemail);
+app.get('/student/', (req, res) => {
+  const userQuery = "SELECT * FROM heroku_dd8592f0f69936c.user";
+  // const userQuery = "SELECT user.id FROM saja_academy.user WHERE user.email =" + connection.escape(req.params.studentemail);
   connection.query(userQuery, function(err, result, fields) {
     if(!err || result.length > 0){
       res.json({result});
@@ -37,6 +38,6 @@ app.get('/student/:studentemail', (req, res) => {
   });
 });
 
-app.listen("https://pure-retreat-21164.herokuapp.com/", () => {
+app.listen('https://pure-retreat-21164.herokuapp.com', () => {
   console.log(`Find the server at: http://localhost:${port}/`); // eslint-disable-line no-console
 });
