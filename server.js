68be5c89c20b55a457a0d36dd7097dcc5dee5819
April Trull <mtwoolybunnies@gmail.com>
2017-09-02 16:33:04 +0100

    sql integratioin attemp 8

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use((req, res, next) =>{
 });
 
 const host = "us-cdbr-iron-east-05.cleardb.net"
-const user = "bed60c6ac47294"
-const pswd = "b11032f8"
-const dbname = "heroku_dd8592f0f69936c"
+const user = "b950c751139083"
+const pswd = "460b17d4"
+const dbname = "heroku_9f0c20bb2fdcf0f"
 
 // config db ====================================
 const connection = mysql.createConnection({
@@ -28,16 +28,17 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-app.get('/student/', (req, res) => {
-  const userQuery = "SELECT * FROM heroku_dd8592f0f69936c.user";
-  // const userQuery = "SELECT user.id FROM saja_academy.user WHERE user.email =" + connection.escape(req.params.studentemail);
-  connection.query(userQuery, function(err, result, fields) {
-    if(!err || result.length > 0){
-      res.json({result});
-    }
-  });
+app.get('/', (req, res) => {
+  res.send('this is the api')
+  // const userQuery = "SELECT * FROM heroku_dd8592f0f69936c.user";
+  // // const userQuery = "SELECT user.id FROM saja_academy.user WHERE user.email =" + connection.escape(req.params.studentemail);
+  // connection.query(userQuery, function(err, result, fields) {
+  //   if(!err || result.length > 0){
+  //     res.json({result});
+  //   }
+  // });
 });
-
-app.listen('https://pure-retreat-21164.herokuapp.com', () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
   console.log(`Find the server at: http://localhost:${port}/`); // eslint-disable-line no-console
 });
