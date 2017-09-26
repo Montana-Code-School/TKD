cb18dbdf21c03209d0384dd23c736c7320c448ce
April Trull <mtwoolybunnies@gmail.com>
2017-09-26 18:50:00 +0100

    really, nothing new

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'saja.auth0.com',
-  clientId: 'WDnb8rFdas0Gok4zE7DhS8RW4HB6z4yR	',
-  callbackUrl: 'https://saja-academy/herokuapp.com/callback'
+  clientId: 'WDnb8rFdas0Gok4zE7DhS8RW4HB6z4yR',
+  callbackUrl: 'https://saja-academy/herokuapp.com/login'
 }
